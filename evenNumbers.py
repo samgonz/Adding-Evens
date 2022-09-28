530a01f6d9fd061156c78d24b99095069e7fdb93
samgonz <doomteam1@gmail.com>
2022-09-27 23:12:57 -0500

    initial Commit

--- evenNumbers.py
+++ evenNumbers.py
@@ -1,1 +1,9 @@
+sumOfEvenNumbers = 0
 
+startingNumber = int(input('What would you like to choose for a starting number?\n'))
+endingNumber = int(input('What would you like to choose for an ending number?\n'))
+for i in range(startingNumber, endingNumber+1):
+    if i % 2 == 0:
+        sumOfEvenNumbers += i
+        
+print(f'The sum of all even numbers from {startingNumber} to {endingNumber} is {sumOfEvenNumbers}.')
